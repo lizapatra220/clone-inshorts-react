516948f604a3fcf63361511fe72660abb6f13dee
lizapatra220 <lizapatra220@gmail.com>
2022-09-23 23:42:44 +0530

    dark theme

--- src/Components/Humburgerinshots.js
+++ src/Components/Humburgerinshots.js
@@ -1,10 +1,10 @@
 import React from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
+import Divider from "@mui/material/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
